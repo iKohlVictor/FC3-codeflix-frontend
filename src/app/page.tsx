e569e0c8067a695a78519f53af8fcf2b3d0f6ac9
ian-kohl <ianvictor181099@gmail.com>
2024-07-03 23:17:24 -0300

    feat: component header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
+import React from "react"
+import Header from './components/Header';
 
 export default function Home() {
   return (
     <div className="relative h-screen lg:h-[140vh] overflow-hidden bg-gradient-to-b">
+      <Header/>
       <main className="relative pb-24 pl-4 lg:pl-16">
         <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
           <div className="absolute flex flex-col left-0 top-0 -z-10 h-[95vh] w-screen bg-black">
@@ -12,10 +15,6 @@ export default function Home() {
           <h1 className="text-lg font-bold md:text-4xl lg:text-7xl">
             MAID
           </h1>
-
-          <p className="text-shadow-md max-w-xs text-xs nd:max-w-lg md:text-lg lg:max-w-2xl">
-            In a future where humans can transfer their minds to robots, a cop and
-          </p>
         </div>
 
         <div className="flex space-x-3">
@@ -29,6 +28,7 @@ export default function Home() {
         </div>
 
       </main>
+
     </div>
   );
 }
